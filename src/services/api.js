c2425ea77471fd6328bfcfb1b2f446f08ddf1336
Ananthakrishna <ananthaak24@gmail.com>
2024-07-28 14:26:37 +0530

    removed the temp folders in backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,12 +1,12 @@
 //For Development
-// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:7222/api';
-const API_BASE_URL = 'https://portfolio-v1-nk24.onrender.com';
+ const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:7222/api';
+//const API_BASE_URL = 'https://portfolio-v1-nk24.onrender.com';
 
 export const sendUserData = async (userData) => {
   try {
     //For Development
-    //const response = await fetch(`${API_BASE_URL}/create-portfolio`, {
-    const response = await fetch(`${API_BASE_URL}/api/create-portfolio`, {
+    const response = await fetch(`${API_BASE_URL}/create-portfolio`, {
+    //const response = await fetch(`${API_BASE_URL}/api/create-portfolio`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
